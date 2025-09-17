1548da8e8c37a0844160a0f5ff80f1e897d23665
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2025-09-17 17:08:11 +0200

    filter connect tags

--- R/utils.R
+++ R/utils.R
@@ -10,14 +10,36 @@ pin_versions <- function(name, board) {
     error = cnd_to_notif()
   )
 
-  if (is.null(res)) {
+  if (is.null(res) || nrow(res) == 0L) {
     return(character())
   }
 
-  set_names(
-    res$version,
-    paste0(res$created, " (", res$hash, ")")
+  if ("hash" %in% colnames(res)) {
+    names <- paste0(res$created, " (", res$hash, ")")
+  } else {
+    names <- res$created
+  }
+
+  set_names(res$version, names)
+}
+
+pin_list <- function(backend) {
+  tryCatch(
+    {
+      res <- pins::pin_list(backend)
+
+      if (inherits(backend, "pins_board_connect")) {
+        res[lgl_ply(res, has_tags, backend)]
+      }
+
+      res
+    },
+    error = cnd_to_notif()
   )
+}
+
+has_tags <- function(x, backend, tags = blockr_session_tags()) {
+  all(tags %in% pins::pin_meta(backend, x)[["tag"]])
 }
 
 cnd_to_notif <- function(return_val = NULL, type = "warning",
@@ -33,3 +55,5 @@ cnd_to_notif <- function(return_val = NULL, type = "warning",
     return_val
   }
 }
+
+blockr_session_tags <- function() "blockr-session"
