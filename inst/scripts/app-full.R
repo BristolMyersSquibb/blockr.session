abc355a6f024d425f3c18ff3e077ddbcabe474f7
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2026-02-26 08:47:59 +0000

    add dag to the full demo app

--- inst/scripts/app-full.R
+++ inst/scripts/app-full.R
@@ -1,6 +1,4 @@
-library(blockr)
-library(blockr.session)
-library(connectapi)
+library(blockr.core)
 
 options(
   blockr.session_mgmt_backend = function() {
@@ -17,10 +15,11 @@ options(
       server = Sys.getenv("CONNECT_SERVER"),
       key = con$api_key
     )
-  }
+  },
+  g6R.layout_on_data_change = TRUE
 )
 
 serve(
-  new_dock_board(),
-  plugins = custom_plugins(manage_project())
+  blockr.dock::new_dock_board(extensions = blockr.dag::new_dag_extension()),
+  plugins = custom_plugins(blockr.session::manage_project())
 )
